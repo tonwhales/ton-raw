056847bd2903ccc3a46f334311d687bc3b749f68
Steve Korshakov <steve@korshakov.com>
2022-04-26 18:44:30 +0400

    fix: fix latest

--- src/api/handlers/handleGetBlockLatest.ts
+++ src/api/handlers/handleGetBlockLatest.ts
@@ -5,7 +5,7 @@ import { warn } from "../../utils/log";
 export function handleGetBlockLatest(): express.RequestHandler {
     return async (req, res) => {
         try {
-            let state = await getSyncState('blocks_fresh');
+            let state = await getSyncState('blocks_simple');
             if (!state) {
                 res.status(500).send('500 Internal Error');
                 return;
